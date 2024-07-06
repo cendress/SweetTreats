c9a398901904e4485e75e287277f040fd9626c87
cendress <95492688+cendress@users.noreply.github.com>
2024-07-06 14:04:08 -0400

    Create Meal model

---
+++ SweetTreats/Meal.swift
@@ -0,0 +1,19 @@
+//
+//  Meal.swift
+//  SweetTreats
+//
+//  Created by Christopher Endress on 7/6/24.
+//
+
+import Foundation
+
+struct Meal: Identifiable, Codable {
+    var idMeal: String
+    var strMeal: String
+    var strMealThumb: String
+    
+    // ID property required to conform to Identifiable protocol. Return the actual ID the API provides.
+    var id: String {
+        return idMeal
+    }
+}
