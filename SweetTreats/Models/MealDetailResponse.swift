9f40aecc00c3afeff00ae311bb3990cbd562e955
cendress <95492688+cendress@users.noreply.github.com>
2024-07-06 14:18:04 -0400

    Create MealDetailResponse that will store an array of MealDetails

---
+++ SweetTreats/Models/MealDetailResponse.swift
@@ -0,0 +1,12 @@
+//
+//  MealDetailResponse.swift
+//  SweetTreats
+//
+//  Created by Christopher Endress on 7/6/24.
+//
+
+import Foundation
+
+struct MealDetailResponse: Codable {
+    var meals: [MealDetail]
+}
