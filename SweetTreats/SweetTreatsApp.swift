7489ded22feb0f89d324fa88e60566b28e5df548
cendress <95492688+cendress@users.noreply.github.com>
2024-07-06 14:00:05 -0400

    Initial Commit

---
+++ SweetTreats/SweetTreatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetTreatsApp.swift
+//  SweetTreats
+//
+//  Created by Christopher Endress on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SweetTreatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
