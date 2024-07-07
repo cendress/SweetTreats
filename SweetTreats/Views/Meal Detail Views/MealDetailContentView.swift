2a332426fa074f87319617d5987dc3964fe991f6
cendress <95492688+cendress@users.noreply.github.com>
2024-07-07 09:47:21 -0400

    Put meal detail content in separate reusable view

--- SweetTreats/Views/Meal Detail Views/MealDetailContentView.swift
+++ SweetTreats/Views/Meal Detail Views/MealDetailContentView.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct MealDetailContentView: View {
+    let meal: MealResponse.Meal
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            if let url = URL(string: meal.strMealThumb) {
+                AsyncImage(url: url) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipped()
+                            .cornerRadius(8)
+                    } else if phase.error != nil {
+                        Image(systemName: "photo")
+                            .background(Color.gray)
+                            .cornerRadius(8)
+                    } else {
+                        ProgressView()
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 50)
+            }
+            
+            Text("Instructions")
+                .font(.headline)
+                .padding([.top, .horizontal])
+            Text(meal.strInstructions ?? "No instructions available.")
+                .padding([.horizontal, .bottom])
+            
+            Text("Ingredients")
+                .font(.headline)
+                .padding([.top, .horizontal])
+            ForEach(meal.ingredients(), id: \.self) { ingredient in
+                Text(ingredient)
+                    .padding(.horizontal)
+            }
+            
+            Text("Measurements")
+                .font(.headline)
+                .padding([.top, .horizontal])
+            ForEach(meal.measurements(), id: \.self) { measurement in
+                Text(measurement)
+                    .padding(.horizontal)
+            }
+        }
     }
 }
 
-#Preview {
-    MealDetailContentView()
-}
+//#Preview {
+//    MealDetailContentView()
+//}
--- SweetTreats/Views/Meal Detail Views/MealDetailView.swift
+++ SweetTreats/Views/Meal Detail Views/MealDetailView.swift
@@ -15,54 +15,14 @@ struct MealDetailView: View {
         NavigationStack {
             ScrollView {
                 if let meal = viewModel.mealDetail {
-                    VStack(alignment: .leading) {
-                        if let url = URL(string: meal.strMealThumb) {
-                            AsyncImage(url: url) { phase in
-                                if let image = phase.image {
-                                    image
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .clipped()
-                                        .cornerRadius(8)
-                                } else if phase.error != nil {
-                                    Image(systemName: "photo")
-                                        .background(Color.gray)
-                                        .cornerRadius(8)
-                                } else {
-                                    ProgressView()
-                                }
-                            }
-                            .frame(maxWidth: .infinity)
-                            .padding(.horizontal, 50)
-                        }
-                        
-                        Text("Instructions")
-                            .font(.headline)
-                            .padding([.top, .horizontal])
-                        Text(meal.strInstructions ?? "No instructions available.")
-                            .padding([.horizontal, .bottom])
-                        
-                        Text("Ingredients")
-                            .font(.headline)
-                            .padding([.top, .horizontal])
-                        ForEach(meal.ingredients(), id: \.self) { ingredient in
-                            Text(ingredient)
-                                .padding(.horizontal)
-                        }
-                        
-                        Text("Measurements")
-                            .font(.headline)
-                            .padding([.top, .horizontal])
-                        ForEach(meal.measurements(), id: \.self) { measurement in
-                            Text(measurement)
-                                .padding(.horizontal)
-                        }
-                    }
+                    MealDetailContentView(meal: meal)
                 } else if viewModel.isLoading {
                     ProgressView()
+                        .frame(maxWidth: .infinity)
                 } else if let errorMessage = viewModel.errorMessage {
                     Text(errorMessage)
                         .padding()
+                        .frame(maxWidth: .infinity)
                 }
             }
             .navigationTitle(viewModel.mealDetail?.strMeal ?? "Loading...")
