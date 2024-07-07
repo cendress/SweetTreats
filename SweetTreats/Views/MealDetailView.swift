1cc8f278664a832c04c7d04ed75177b63821beee
cendress <95492688+cendress@users.noreply.github.com>
2024-07-07 09:38:54 -0400

    Complete MealDetailView

--- SweetTreats/Views/MealDetailView.swift
+++ SweetTreats/Views/MealDetailView.swift
@@ -8,30 +8,74 @@
 import SwiftUI
 
 struct MealDetailView: View {
+    @StateObject private var viewModel = MealDetailViewModel()
     var mealID: String
-
+    
     var body: some View {
         NavigationStack {
             ScrollView {
-                VStack(alignment: .leading) {
-                    
-                    // Maybe place image thumbnail here?
-                    
-                    Text("Instructions will go here.")
-                        .padding()
-                    
-                    Text("Ingredients will go here.")
-                        .padding()
-                    
-                    Text("Measurements will go here.")
+                if let meal = viewModel.mealDetail {
+                    VStack(alignment: .leading) {
+                        if let url = URL(string: meal.strMealThumb) {
+                            AsyncImage(url: url) { phase in
+                                if let image = phase.image {
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .clipped()
+                                        .cornerRadius(8)
+                                } else if phase.error != nil {
+                                    Image(systemName: "photo")
+                                        .background(Color.gray)
+                                        .cornerRadius(8)
+                                } else {
+                                    ProgressView()
+                                }
+                            }
+                            .frame(maxWidth: .infinity)
+                            .padding(.horizontal, 50)
+                        }
+                        
+                        Text("Instructions")
+                            .font(.headline)
+                            .padding([.top, .horizontal])
+                        Text(meal.strInstructions ?? "No instructions available.")
+                            .padding([.horizontal, .bottom])
+                        
+                        Text("Ingredients")
+                            .font(.headline)
+                            .padding([.top, .horizontal])
+                        ForEach(meal.ingredients(), id: \.self) { ingredient in
+                            Text(ingredient)
+                                .padding(.horizontal)
+                        }
+                        
+                        Text("Measurements")
+                            .font(.headline)
+                            .padding([.top, .horizontal])
+                        ForEach(meal.measurements(), id: \.self) { measurement in
+                            Text(measurement)
+                                .padding(.horizontal)
+                        }
+                    }
+                } else if viewModel.isLoading {
+                    ProgressView()
+                } else if let errorMessage = viewModel.errorMessage {
+                    Text(errorMessage)
                         .padding()
                 }
             }
-            .navigationTitle(mealID)
+            .navigationTitle(viewModel.mealDetail?.strMeal ?? "Loading...")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                Task {
+                    await viewModel.loadMealDetails(mealID: mealID)
+                }
+            }
         }
     }
 }
 
 #Preview {
-    MealDetailView(mealID: "Sample Dessert Name")
+    MealDetailView(mealID: "52772")
 }
