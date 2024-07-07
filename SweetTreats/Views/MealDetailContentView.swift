c458192346da169bfa326f366b7b03dabec62225
cendress <95492688+cendress@users.noreply.github.com>
2024-07-07 09:41:45 -0400

    Create MealDetailContentView

---
+++ SweetTreats/Views/MealDetailContentView.swift
@@ -0,0 +1,18 @@
+//
+//  MealDetailContentView.swift
+//  SweetTreats
+//
+//  Created by Christopher Endress on 7/7/24.
+//
+
+import SwiftUI
+
+struct MealDetailContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealDetailContentView()
+}
